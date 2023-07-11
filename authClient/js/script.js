7b6f17289dee6e4da7d41694394ab858bf145756
Hencsat <name66437@gmail.com>
2023-07-11 14:38:33 +0300

    changes

--- authClient/js/script.js
+++ authClient/js/script.js
@@ -1,12 +1,18 @@
 const url = "http://worldtimeapi.org/api/timezone/Europe/Moscow"
 const localhost = "http://localhost:5050"
 
+const button = document.getElementsByClassName("sign_up_button")[0]
+
 async function getData() {
     const response = await fetch(localhost)
     console.log(await response.text())
 }
 
-getData()
+button.addEventListener("click", (event) => {
+    getData()
+})
+
+//getData() 
 
 
 
