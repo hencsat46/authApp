4b309407d38bb66f8d07c1b252e772ca7b435cdc
hencsat <name66437@gmail.com>
2023-07-11 13:20:03 +0300

    fetch done

--- authClient/js/script.js
+++ authClient/js/script.js
@@ -1,12 +1,12 @@
+const url = "http://worldtimeapi.org/api/timezone/Europe/Moscow"
+const localhost = "http://localhost:5050"
 
-let btn = document.getElementsByClassName("sign_up_button")[0]
+async function getData() {
+    const response = await fetch(localhost)
+    console.log(await response.text())
+}
 
-
-btn.addEventListener("click", (event) => {
-    const response = fetch("http://127.0.0.1:5050")
-    console.log(response)
-    
-})
+getData()
 
 
 
